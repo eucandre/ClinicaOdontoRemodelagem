5dbfeb9ce41cb6601e54399cf1b439b234b2baab
carlos <eucandre@gmail.com>
2017-11-05 13:47:36 -0300

    ajustes ao dashboar com os módulos da app_base

--- app_base/admin.py
+++ app_base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 admin.site.register(Cliente)
 admin.site.register(Profissional)
+admin.site.register(Funcionario)
 admin.site.register(Marca)
 admin.site.register(Produto)
 admin.site.register(Contas_a_Pagar)
--- app_base/apresentacao.py
+++ app_base/apresentacao.py
@@ -19,5 +19,34 @@ def apresenta(request):
     obj_prod= Produto.objects.all()
     obj_cnta= Contas_a_Pagar.objects.all()
     obj_cntp= Contas_a_Pagar_Pagas.objects.all()
+    
+    soma_fun = 0
+    soma_prof = 0
+    soma_produ = 0
+    if len(obj_func)==0:
+        obj_func_data = 0
+    
+    if len(obj_func)>0:  
+        for i in range(len(obj_func)):  
+            soma_fun = Funcionario.objects.get(pk=len(obj_func)).salary + Funcionario.objects.get(pk=len(obj_func)-1).salary
+            obj_func_data = soma_fun
 
-    return render(request,'index.html')
+    if len(obj_prof)==0:
+        obj_prof_data = 0
+
+    if len(obj_prof)>0:  
+        for i in range(len(obj_prof)):  
+            soma_prof = Profissional.objects.get(pk=len(obj_prof)).salary + Profissional.objects.get(pk=len(obj_prof)-1).salary
+            obj_prof_data = soma_prof
+    
+    if len(obj_clie) == 0:
+        obj_clie_data = 0
+
+    if len(obj_produ) > 0:
+        soma_produ =  Produto.objects.get(pk=len(obj_clie)).salary + Produto.objects.get(pk=len(obj_clie)-1).salary
+        obj_clie_data = soma_cli
+
+    return render(request,'index.html',{'funcionario':len(obj_func), 'profissionais':len(obj_prof),
+        'cliente':len(obj_clie), 'marcas':len(obj_marc),'fornecedor':len(obj_forn),'produto':len(obj_prod),
+        'contas_a_pagar':len(obj_cnta),'contas_pagas':len(obj_cntp), 
+        'valor_func':obj_func_data, 'valor_prof':obj_prof_data, })
